686f90b1baeed2335d3e3ddf3be751c597683dbf
Khiem-Kim Ho Xuan <khiem_89@hotmail.com>
2015-02-24 22:00:43 +0100

    fixed indented project

--- tagstory/src/main/java/no/tagstory/StoryDetailActivity.java
+++ tagstory/src/main/java/no/tagstory/StoryDetailActivity.java
@@ -28,192 +28,192 @@ import java.io.FileNotFoundException;
 
 public class StoryDetailActivity extends Activity {
 
-	private final static int ENABLE_GPS = 1001;
+    private final static int ENABLE_GPS = 1001;
 
-	protected String story_id;
-	protected StoryManager storyManager;
-	protected Story story;
-	protected AlertDialog enableGPSDialog, enableNFCDialog, enableQRDialog;
-	protected StoryApplication storyApplication;
+    protected String story_id;
+    protected StoryManager storyManager;
+    protected Story story;
+    protected AlertDialog enableGPSDialog, enableNFCDialog, enableQRDialog;
+    protected StoryApplication storyApplication;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_story_detail);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_story_detail);
 
-		storyManager = new StoryManager(this);
-		storyApplication = (StoryApplication) getApplication();
+        storyManager = new StoryManager(this);
+        storyApplication = (StoryApplication) getApplication();
 
-		story_id = getIntent().getStringExtra(Database.STORY_ID);
-		if (story_id != null) {
-			story = storyManager.getStory(story_id);
-		} else {
-			story = (Story) getIntent().getSerializableExtra(
-					StoryActivity.EXTRA_STORY);
-		}
+        story_id = getIntent().getStringExtra(Database.STORY_ID);
+        if (story_id != null) {
+            story = storyManager.getStory(story_id);
+        } else {
+            story = (Story) getIntent().getSerializableExtra(
+                    StoryActivity.EXTRA_STORY);
+        }
 
-		if (story != null) {
-			boolean showDefault = false;
-			setTitle(story.getTitle());
-			((TextView) findViewById(R.id.story_detail_desc)).setText(story
-					.getDescription());
+        if (story != null) {
+            boolean showDefault = false;
+            setTitle(story.getTitle());
+            ((TextView) findViewById(R.id.story_detail_desc)).setText(story
+                    .getDescription());
 
-			String imagefile = story.getImage();
-			if (imagefile != null
-					&& imagefile.length() != 0) {
-				try {
-					Bitmap myBitmap = BitmapFactory.decodeStream(openFileInput(imagefile));
-					((ImageView) findViewById(R.id.story_detail_image))
-							.setImageBitmap(myBitmap);
-				} catch (FileNotFoundException e) {
-					showDefault = true;
-				}
-			} else {
-				showDefault = true;
-			}
+            String imagefile = story.getImage();
+            if (imagefile != null
+                    && imagefile.length() != 0) {
+                try {
+                    Bitmap myBitmap = BitmapFactory.decodeStream(openFileInput(imagefile));
+                    ((ImageView) findViewById(R.id.story_detail_image))
+                            .setImageBitmap(myBitmap);
+                } catch (FileNotFoundException e) {
+                    showDefault = true;
+                }
+            } else {
+                showDefault = true;
+            }
 
-			if (showDefault) {
-				((ImageView) findViewById(R.id.story_detail_image))
-						.setImageResource(R.drawable.placeimg_960_720_nature_1);
-			}
-		}
-	}
+            if (showDefault) {
+                ((ImageView) findViewById(R.id.story_detail_image))
+                        .setImageResource(R.drawable.placeimg_960_720_nature_1);
+            }
+        }
+    }
 
-	public void startStory(View v) {
-		if (v.getId() == R.id.start_story_button) {
-			if (hasPhoneRequirements()) {
-				return;
-			}
-			startStory();
-		}
-	}
+    public void startStory(View v) {
+        if (v.getId() == R.id.start_story_button) {
+            if (hasPhoneRequirements()) {
+                return;
+            }
+            startStory();
+        }
+    }
 
-	private boolean hasPhoneRequirements() {
-		String mode = PhoneRequirementsUtils.checkGamemodes(this, story.getGameModes());
-		if (mode != null) {
-			// No game mode requirements, yet
-		}
+    private boolean hasPhoneRequirements() {
+        String mode = PhoneRequirementsUtils.checkGamemodes(this, story.getGameModes());
+        if (mode != null) {
+            // No game mode requirements, yet
+        }
 
-		TagTypeEnum type = PhoneRequirementsUtils.checkTagtypes(this, story.getTagTypes());
-		if (type != null) {
-			if (type.isQR()) {
-				showNoQrDialog();
-			} else if (type.isGPS()) {
-				showNoGpsDialog();
-			} else if (type.isNFC()) {
-				showNoNfcDialog();
-			}
+        TagTypeEnum type = PhoneRequirementsUtils.checkTagtypes(this, story.getTagTypes());
+        if (type != null) {
+            if (type.isQR()) {
+                showNoQrDialog();
+            } else if (type.isGPS()) {
+                showNoGpsDialog();
+            } else if (type.isNFC()) {
+                showNoNfcDialog();
+            }
 
-			return true;
-		}
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	protected void startStory() {
-		storyApplication.startStory(story);
-		startStoryActivity();
-	}
+    protected void startStory() {
+        storyApplication.startStory(story);
+        startStoryActivity();
+    }
 
-	protected void startStoryActivity() {
-		Intent intent = new Intent(this, StoryActivity.class);
-		intent.putExtra(StoryActivity.EXTRA_STORY, story);
-		intent.putExtra(StoryActivity.EXTRA_TAG, story.getStartTagId());
-		startActivity(intent);
-	}
+    protected void startStoryActivity() {
+        Intent intent = new Intent(this, StoryActivity.class);
+        intent.putExtra(StoryActivity.EXTRA_STORY, story);
+        intent.putExtra(StoryActivity.EXTRA_TAG, story.getStartTagId());
+        startActivity(intent);
+    }
 
-	private void showNoQrDialog() {
-		if (enableQRDialog == null) {
-			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(getResources().getString(
-					R.string.dialog_download_barcodescanner));
-			builder.setCancelable(false);
-			builder.setPositiveButton(R.string.dialog_go_to_play_store, new DialogInterface.OnClickListener() {
-				public void onClick(final DialogInterface dialog, final int id) {
-					String appPackageName = getString(R.string.barcode_scanner_play_store);
-					try {
-						startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
-					} catch (android.content.ActivityNotFoundException anfe) {
-						startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
-					}
-				}
-			});
-			builder.setNegativeButton(R.string.cancel,
-					new DialogInterface.OnClickListener() {
-						public void onClick(final DialogInterface dialog,
-						                    final int id) {
-							dialog.cancel();
-						}
-					});
-			enableQRDialog = builder.create();
-		}
-		enableQRDialog.show();
-	}
+    private void showNoQrDialog() {
+        if (enableQRDialog == null) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(getResources().getString(
+                    R.string.dialog_download_barcodescanner));
+            builder.setCancelable(false);
+            builder.setPositiveButton(R.string.dialog_go_to_play_store, new DialogInterface.OnClickListener() {
+                public void onClick(final DialogInterface dialog, final int id) {
+                    String appPackageName = getString(R.string.barcode_scanner_play_store);
+                    try {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+                    } catch (android.content.ActivityNotFoundException anfe) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+                    }
+                }
+            });
+            builder.setNegativeButton(R.string.cancel,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog,
+                                            final int id) {
+                            dialog.cancel();
+                        }
+                    });
+            enableQRDialog = builder.create();
+        }
+        enableQRDialog.show();
+    }
 
-	private void showNoNfcDialog() {
-		if (enableNFCDialog == null) {
-			// TODO
-		}
-	}
+    private void showNoNfcDialog() {
+        if (enableNFCDialog == null) {
+            // TODO
+        }
+    }
 
-	protected void showNoGpsDialog() {
-		if (enableGPSDialog == null) {
-			final AlertDialog.Builder builder = new AlertDialog.Builder(this);
-			builder.setMessage(getResources().getString(
-					R.string.dialog_enable_gps));
-			builder.setCancelable(false);
-			builder.setPositiveButton(R.string.yes,
-					new DialogInterface.OnClickListener() {
-						public void onClick(final DialogInterface dialog,
-						                    final int id) {
+    protected void showNoGpsDialog() {
+        if (enableGPSDialog == null) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage(getResources().getString(
+                    R.string.dialog_enable_gps));
+            builder.setCancelable(false);
+            builder.setPositiveButton(R.string.yes,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog,
+                                            final int id) {
 
-							startActivityForResult(new Intent(
-											Settings.ACTION_LOCATION_SOURCE_SETTINGS),
-									ENABLE_GPS);
-						}
-					});
-			builder.setNegativeButton(R.string.no,
-					new DialogInterface.OnClickListener() {
-						public void onClick(final DialogInterface dialog,
-						                    final int id) {
-							dialog.cancel();
-						}
-					});
-			enableGPSDialog = builder.create();
-		}
-		enableGPSDialog.show();
-	}
+                            startActivityForResult(new Intent(
+                                            Settings.ACTION_LOCATION_SOURCE_SETTINGS),
+                                    ENABLE_GPS);
+                        }
+                    });
+            builder.setNegativeButton(R.string.no,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(final DialogInterface dialog,
+                                            final int id) {
+                            dialog.cancel();
+                        }
+                    });
+            enableGPSDialog = builder.create();
+        }
+        enableGPSDialog.show();
+    }
 
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		if (resultCode == ENABLE_GPS) {
-			startStory();
-		}
-	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == ENABLE_GPS) {
+            startStory();
+        }
+    }
 
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.story_detail, menu);
-		return true;
-	}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.story_detail, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onMenuItemSelected(int featureId, MenuItem item) {
-		switch (item.getItemId()) {
-			case R.id.menu_delete_story:
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_delete_story:
                 storyManager.deleteAllAssetsRelativeToStoryAndItself(story_id);
                 if (storyManager.deleteStory(story_id)
-                        && deleteFile(story_id+".json")) {
+                        && deleteFile(story_id + ".json")) {
                     //TODO add shared pref
                     finish();
-				} else {
-					Toast.makeText(this, "Story was not deleted, please try again.", Toast.LENGTH_SHORT).show();
-				}
-				break;
-		}
+                } else {
+                    Toast.makeText(this, "Story was not deleted, please try again.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
 
-		return true;
-	}
+        return true;
+    }
 }
--- tagstory/src/main/java/no/tagstory/story/StoryManager.java
+++ tagstory/src/main/java/no/tagstory/story/StoryManager.java
@@ -23,43 +23,43 @@ import java.util.regex.Pattern;
 
 public class StoryManager {
 
-	private Database database;
-	private Context context;
+    private Database database;
+    private Context context;
 
-	public StoryManager(Context context) {
-		this.context = context;
-		database = new Database(this.context);
-		database.open();
-	}
+    public StoryManager(Context context) {
+        this.context = context;
+        database = new Database(this.context);
+        database.open();
+    }
 
-	public void closeDatabase() {
-		database.close();
-	}
+    public void closeDatabase() {
+        database.close();
+    }
 
-	public Cursor getCursorOverStories() {
-		// TODO Convert cursor to a StoryList-object and return a list
-		return database.getStoryList();
-	}
+    public Cursor getCursorOverStories() {
+        // TODO Convert cursor to a StoryList-object and return a list
+        return database.getStoryList();
+    }
 
-	public Story getStory(String id) {
-		try {
-			return StoryParser.parseJsonToStory(context, id);
-		} catch (JSONException | IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+    public Story getStory(String id) {
+        try {
+            return StoryParser.parseJsonToStory(context, id);
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	public boolean deleteStory(String id) {
-		return database.deleteStory(id);
-	}
+    public boolean deleteStory(String id) {
+        return database.deleteStory(id);
+    }
 
-	public boolean hasStory(String id) {
-		return database.hasStory(id);
-	}
+    public boolean hasStory(String id) {
+        return database.hasStory(id);
+    }
 
     public boolean deleteAllAssetsRelativeToStoryAndItself(String id) {
-        if(hasStory(id)) {
+        if (hasStory(id)) {
             try {
                 String mFileName = id.concat(".json");
                 String mParsedLine;
@@ -70,33 +70,33 @@ public class StoryManager {
                 BufferedReader mBufferedReader = new BufferedReader(mInputStreamReader);
                 mParsedLine = mBufferedReader.readLine();
                 JSONObject mStoryObject = new JSONObject(mParsedLine).getJSONObject(StoryParser.STORY);
-                if(mStoryObject.optString(StoryParser.IMAGE,"") != null) {
-                    context.deleteFile((String)mStoryObject.get(StoryParser.IMAGE));
+                if (mStoryObject.optString(StoryParser.IMAGE, "") != null) {
+                    context.deleteFile((String) mStoryObject.get(StoryParser.IMAGE));
                 }
 
                 JSONObject mTagObjects = mStoryObject.getJSONObject(StoryParser.TAGS);
                 Iterator<String> mKeys = mTagObjects.keys();
-                while(mKeys.hasNext()) {
+                while (mKeys.hasNext()) {
                     String mKey = mKeys.next();
                     JSONObject mTag = mTagObjects.getJSONObject(mKey);
 
-                    if(mTag.has(StoryParser.TAG_OPTIONS)) {
+                    if (mTag.has(StoryParser.TAG_OPTIONS)) {
                         JSONArray mOptions = mTag.getJSONArray(StoryParser.TAG_OPTIONS);
-                        for(int index = 0; index < mOptions.length(); index++) {
+                        for (int index = 0; index < mOptions.length(); index++) {
                             JSONObject mOption = mOptions.getJSONObject(index);
                             if (mOption.has(StoryParser.HINT_IMAGE_SOURCE)) {
-                                context.deleteFile((String)mOption.get(StoryParser.HINT_IMAGE_SOURCE));
+                                context.deleteFile((String) mOption.get(StoryParser.HINT_IMAGE_SOURCE));
                             }
-                            if(mOption.has(StoryParser.HINT_SOUND_SOURCE)) {
+                            if (mOption.has(StoryParser.HINT_SOUND_SOURCE)) {
                                 String mFile = (String) mOption.get(StoryParser.HINT_SOUND_SOURCE);
-                                    context.deleteFile(mFile);
+                                context.deleteFile(mFile);
                             }
                         }
                     }
                 }
                 mInputStream.close();
                 return true;
-            }catch(Exception e) {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }

ecdeb046100dfe607a581143317799dffa91d63d
Kyrre Havik Eriksen <Kyrremann@gmail.com>
2015-03-02 19:15:42 +0100

    Retaining camera position on rotation

--- tagstory/src/main/java/no/tagstory/story/activity/option/gps/GPSMapNavigationActivity.java
+++ tagstory/src/main/java/no/tagstory/story/activity/option/gps/GPSMapNavigationActivity.java
@@ -1,48 +1,81 @@
 package no.tagstory.story.activity.option.gps;
 
 import android.os.Bundle;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import no.tagstory.R;
+import no.tagstory.story.activity.option.MapNavigationActivity;
+
+import static no.tagstory.story.activity.option.MapNavigationActivity.*;
 
 public class GPSMapNavigationActivity extends GPSActivity {
 
 	private SupportMapFragment mapView;
+	private GoogleMap googleMap;
+	private CameraPosition lastCameraPosition;
 
 	@Override
-	protected void onCreate(Bundle arg0) {
-		super.onCreate(arg0);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_story_map);
+
+		mapView = (SupportMapFragment) getSupportFragmentManager()
+				.findFragmentById(R.id.story_map_view);
+
+		googleMap = mapView.getMap();
+		googleMap.setMyLocationEnabled(true);
+		UiSettings uiSettings = googleMap.getUiSettings();
+		uiSettings.setMyLocationButtonEnabled(true);
+		uiSettings.setZoomControlsEnabled(true);
+
+		if (savedInstanceState != null) {
+			float zoom = savedInstanceState.getFloat(ZOOM);
+			double latitude = savedInstanceState.getDouble(LATITUDE);
+			double longitue = savedInstanceState.getDouble(LONGITUDE);
+			LatLng latLng = new LatLng(latitude, longitue);
+			lastCameraPosition = CameraPosition.fromLatLngZoom(latLng, zoom);
+		}
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
+		LatLng latLng = new LatLng(option.getLatitude(), option.getLongitude());
+		CameraUpdate cameraUpdate;
 
-		mapView = (SupportMapFragment) getSupportFragmentManager()
-				.findFragmentById(R.id.story_map_view);
-		GoogleMap googleMap = mapView.getMap();
-		googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-		googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(
-				option.getLatitude(), option.getLongitude()), option.getZoomLevel()));
-		googleMap.setMyLocationEnabled(true);
+		if (lastCameraPosition == null) {
+			cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, option.getZoomLevel());
+		} else {
+			cameraUpdate = CameraUpdateFactory.newCameraPosition(lastCameraPosition);
+		}
+		googleMap.moveCamera(cameraUpdate);
 
-		addMarker(googleMap, option.getLatitude(), option.getLongitude(),
-				R.string.dummy_map_target, -1);
+		addMarker(googleMap, latLng, R.string.dummy_map_target, -1);
 	}
 
-	private void addMarker(GoogleMap map, double lat, double lon, int title,
-	                       int snippetId) {
+	// TODO Implement marker snippet in the story json
+	private void addMarker(GoogleMap map, LatLng latLng, int title, int snippetId) {
 		String snippet = "";
 		if (snippetId == -1) {
 			snippet = getString(R.string.dummy_map_snippet);
 		} else {
 			snippet = getString(snippetId);
 		}
-		map.addMarker(new MarkerOptions().position(new LatLng(lat, lon))
-				.title(getString(title)).snippet(snippet));
+		map.addMarker(
+				new MarkerOptions()
+						.position(latLng)
+						.title(getString(title))
+						.snippet(snippet));
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		CameraPosition cameraPosition = googleMap.getCameraPosition();
+		LatLng cameraTarget = cameraPosition.target;
+		outState.putFloat(ZOOM, cameraPosition.zoom);
+		outState.putDouble(LATITUDE, cameraTarget.latitude);
+		outState.putDouble(LONGITUDE, cameraTarget.longitude);
 	}
 }
